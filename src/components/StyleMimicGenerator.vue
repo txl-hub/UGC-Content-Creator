68c4f47829da5d5f36d745043cc09d36b601fc38
tangxiaolong <tangxiaolong@aa.com>
2025-01-16 17:31:48 +0800

    修改后端接口Ip

--- src/components/StyleMimicGenerator.vue
+++ src/components/StyleMimicGenerator.vue
@@ -211,14 +211,14 @@ export default {
           }
           
           console.log('发送请求数据:', {
-            url: "http://localhost:8080/api/content/generate-mimic",
+            url: "http://8.217.72.161:8080/api/content/generate-mimic",
             method: 'POST',
             data: requestData,
             timestamp: new Date().toISOString()
           })
 
           const response = await axios.post(
-            "http://localhost:8080/api/content/generate-mimic", 
+            "http://8.217.72.161:8080/api/content/generate-mimic", 
             requestData
           )
 
