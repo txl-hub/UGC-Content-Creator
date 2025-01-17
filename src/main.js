0303577d223f114a8e223c92e1cf257bba67f205
tangxiaolong <tangxiaolong@aa.com>
2025-01-17 16:15:51 +0800

    前端1

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue';
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'  // 确保引入 Element Plus 的样式
 import App from './App.vue';
-import './styles/theme.css'; // 引入全局样式
+import './styles/theme.css';
 
-createApp(App).mount('#app');
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
+  publicPath: '/',
+  configureWebpack: {
+    output: {
+      filename: 'js/[name].[hash].js',
+      chunkFilename: 'js/[name].[hash].js'
+    }
+  },
+  css: {
+    extract: {
+      filename: 'css/[name].[hash].css',
+      chunkFilename: 'css/[name].[hash].css'
+    }
+  }
 })
