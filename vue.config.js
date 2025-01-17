8335961c0b9e5cb78958cb2089a8f7738292da79
tangxiaolong <tangxiaolong@aa.com>
2025-01-17 17:12:34 +0800

    前端latest

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/',
+  publicPath: './',
   configureWebpack: {
     output: {
       filename: 'js/[name].[hash].js',
